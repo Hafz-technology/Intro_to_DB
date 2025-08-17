d3872c52be2688a2f679e05939186adc3b15953d
Khalil Hafz <khalilhafz@gmail.com>
2025-08-17 19:33:29 +0300

    setup

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -26,28 +26,28 @@ CREATE TABLE IF NOT EXISTS Authors (
 
 -- Create the table for Customers
 CREATE TABLE IF NOT EXISTS Customers (
-    `customer_id` INT NOT NULL AUTO_INCREMENT,
-    `customer_name` VARCHAR(215) NOT NULL,
-    `email` VARCHAR(215) NOT NULL,
-    `address` TEXT,
+    customer_id INT NOT NULL AUTO_INCREMENT,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
+    address TEXT,
     PRIMARY KEY (`customer_id`)
 );
 
 -- Create the table for Orders
 CREATE TABLE IF NOT EXISTS Orders (
-    `order_id` INT NOT NULL AUTO_INCREMENT,
-    `customer_id` INT NOT NULL,
-    `order_date` DATE NOT NULL,
+    order_id INT NOT NULL AUTO_INCREMENT,
+    customer_id INT NOT NULL,
+    order_date DATE NOT NULL,
     PRIMARY KEY (`order_id`),
     FOREIGN KEY (`customer_id`) REFERENCES `Customers`(`customer_id`)
 );
 
 -- Create the table for Order_Details
 CREATE TABLE IF NOT EXISTS Order_Details (
-    `orderdetailid` INT NOT NULL AUTO_INCREMENT,
-    `order_id` INT NOT NULL,
-    `book_id` INT NOT NULL,
-    `quantity` INT NOT NULL,
+    orderdetailid INT NOT NULL AUTO_INCREMENT,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL,
+    quantity INT NOT NULL,
     PRIMARY KEY (`orderdetailid`),
     FOREIGN KEY (`order_id`) REFERENCES `Orders`(`order_id`),
     FOREIGN KEY (`book_id`) REFERENCES `Books`(`book_id`)
